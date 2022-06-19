521031ec0d6a278c60f31b2ba6543fb8c9db9ff2
zahrarahmanifar <100475614+ZahraRahmanifar@users.noreply.github.com>
2022-06-19 10:33:45 +0430

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,8 @@
+new_grammar = {
+" S " : [ " aBcD " " DDA " " bBD " ] ,
+" A " : [ " aAc " " ACA " " λ " " AD " ] , 
+" B " : [ " BC " " AaB " " CAaa " ] ,
+" C " : [ " BC " " AaB " " CAaa " ] ,
+" D " : [ " aDaD " " baba " " baba " ] 
+}
+print (" grammar ": + new_grammar [ " S " ] , [ " A " ] , [ " B " ] , [ " C " ] , [ " D " ] ) I
